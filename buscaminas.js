7427b21e7e592d925f4e5d48e0a39ee68b5dc09f
adriabalmes <aberruez@jaumebalmes.net>
2022-11-10 21:57:02 +0100

    pescaminas parte 2

--- buscaminas.js
+++ buscaminas.js
@@ -4,9 +4,14 @@ for (var i = 0; i < rows.length; i++) {
     matrix.push(rows[i].children)
 }
 
+let minas = [];
+
+
+
 function createTable (){
         let rows = document.getElementById("inputX").valueAsNumber;
         let columns = document.getElementById("inputY").valueAsNumber;
+        
 
         for (let i = 0; i < columns; i++) {
             let filaActual = document.getElementById('table').insertRow(i);
@@ -21,7 +26,8 @@ function createTable (){
         rows = document.querySelector("tbody").children
         for (var i = 0; i < rows.length; i++) {
             matrix.push(rows[i].children);
-    }
+        }
+       // document.getElementById('table').style.border="1";
 }
 
 function matriuBinaria(matrix) {
@@ -31,11 +37,9 @@ function matriuBinaria(matrix) {
         for (var j = 0; j < matrix[0].length; j++) {
             if (matrix[i][j].style.backgroundColor == "red") {
                 rows.push(1);
-                // matrix2[i][j].innerText = 1;
             }
             else {
                 rows.push(0);
-                // matrix2[i][j].innerText = 0;
             }
         }
         matrix2.push(rows); 
@@ -44,38 +48,37 @@ function matriuBinaria(matrix) {
     return matrix2;
 }
 
-function paintCheckerboard() {
-    let rows = document.querySelector("tbody").children
-    let matrix = []
-    for (var i = 0; i < rows.length; i++) {
-        matrix.push(rows[i].children)
-    }
-  //  erase();
-    for (let i = 0; i < matrix.length; i++ ) { // filas
-        if (i%2==0) {
-            for (let j = 0; j < matrix[i].length; j+=2) { // columnas
-                matrix[i][j].style.backgroundColor = "red";
+function inicialitzaMines() {
+        let rows = document.getElementById("inputX").valueAsNumber;
+        let columns = document.getElementById("inputY").valueAsNumber;
+        let nMinas = document.getElementById("nMinas").valueAsNumber;
+        let minasRepartidas = 0;
+
+        for (let i = 0; i < columns; i++) {
+            minas[i] = [];
+            for (let j = 0; j < rows; j++) {
+                minas[i][j] = 0;
             }
         }
-        else {
-            for (let j = 1; j < matrix[i].length; j+=2) { // columnas
+
+        while (minasRepartidas<nMinas){
+            let fila = Math.floor(Math.random() * rows);
+            let columna = Math.floor(Math.random() * columns);
+           
+            if (minas[fila][columna] == 0){
+                minas[fila][columna] = 1;
+                //contador para minas repartidas
+                minasRepartidas++;
+            }
+        }
+
+        for (let i = 0; i < columns; i++) {
+        for (let j = 0; j < rows; j++) {
+            if (minas[i][j] == 1) {
                 matrix[i][j].style.backgroundColor = "red";
             }
         }
     }
 }
 
-function paintAll() {
-  //  erase();
-    let rows = document.querySelector("tbody").children
-    let matrix = []
-    for (var i = 0; i < rows.length; i++) {
-        matrix.push(rows[i].children)
-    }
-    for (let i = 0; i < matrix.length; i++ ) { // filas
-        for (let j = 0; j < matrix[i].length; j++) { // columnas
-            matrix[i][j].style.backgroundColor = "red";
-        }
-    }
-}
 
