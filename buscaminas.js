a690fe42934657a876ceb144031babbfdba50c41
adriabalmes <aberruez@jaumebalmes.net>
2022-11-30 13:21:47 +0100

    pescaminas parte 4

--- buscaminas.js
+++ buscaminas.js
@@ -12,10 +12,14 @@ function createTable (){
         let fila = document.createElement("tr"); // se crea la FILA
         for (let j = 0; j < rows; j++) {
             let celda = document.createElement("td");  // se crea la CELDA
-            celda.style.border = "1px solid";
-            celda.style.width = "12%";
-            celda.innerHTML = "&nbsp";
-            celda.setAttribute("id",  j + " " + i); // asginamos ID
+            celda.style.border = "1px solid grey";
+            celda.style.width = "20px";
+            celda.style.height = "20px";
+            celda.style.textAlign = "center";
+            celda.style.fontWeight = "bold";
+            //celda.setAttribute("id", j + " " + i); // asginamos ID
+            celda.setAttribute("id", j); // asginamos ID X
+            celda.setAttribute("class", i); // asginamos CLASS Y
             fila.appendChild(celda);
         }
         tbody.appendChild(fila);
@@ -27,8 +31,8 @@ function createTable (){
     for (var i = 0; i < rows.length; i++) {
         matrix.push(rows[i].children);
     }
-    // asignamos funcion CLICK para celda
-    tbody.addEventListener("click", coordenadaCelda); 
+    // asignamos funcion CLICK para tbody celda
+    tbody.addEventListener("click", minasAlrededor); 
     // inicializamos la matriz todo a 0
     for (let i = 0; i < columns; i++) {
         minas[i] = [];
@@ -60,29 +64,31 @@ function inicialitzaMines() {
     let columns = document.getElementById("inputY").valueAsNumber;
     let nMinas = document.getElementById("nMinas").valueAsNumber;
     let minasRepartidas = 0;
-    // inicializamos la matriz todo a 0
-    for (let i = 0; i < columns; i++) {
-        minas[i] = [];
-        for (let j = 0; j < rows; j++) {
-            minas[i][j] = 0;
+    if (nMinas <= (rows*columns)){
+        // inicializamos la matriz todo a 0
+        for (let i = 0; i < columns; i++) {
+            minas[i] = [];
+            for (let j = 0; j < rows; j++) {
+                minas[i][j] = 0;
+            }
         }
-    }
-    // repartimos minas en matriz minas[]
-    while (minasRepartidas < nMinas){
-        let fila = Math.floor(Math.random() * rows);
-        let columna = Math.floor(Math.random() * columns);
-        
-        if (minas[fila][columna] == 0){
-            minas[fila][columna] = 1;
-            //contador para minas repartidas
-            minasRepartidas++;
+        // repartimos minas en matriz minas[]
+        while (minasRepartidas < nMinas){
+            let fila = Math.floor(Math.random() * rows);
+            let columna = Math.floor(Math.random() * columns);
+            
+            if (minas[fila][columna] == 0){
+                minas[fila][columna] = 1;
+                //contador para minas repartidas
+                minasRepartidas++;
+            }
         }
-    }
-    // pintamos las minas en la tabla matrix[]
-    for (let i = 0; i < columns; i++) {
-        for (let j = 0; j < rows; j++) {
-            if (minas[i][j] == 1) {
-                matrix[i][j].style.backgroundColor = "red";
+        // pintamos las minas en la tabla matrix[]
+        for (let i = 0; i < columns; i++) {
+            for (let j = 0; j < rows; j++) {
+                if (minas[i][j] == 1) {
+                    matrix[i][j].style.backgroundColor = "red";
+                }
             }
         }
     }
@@ -97,5 +103,63 @@ function coordenadaCelda(event) {
     }
     else {
         return console.log("NO HAY MINA");
+    }
+}
+
+// function paintNeighbours() {
+//     let inputX = document.getElementById("inputX").valueAsNumber;
+//     let inputY = document.getElementById("inputY").valueAsNumber;
+//     //erase();
+//         for (let i = inputY - 1; i <= inputY + 1; i++) { //  Y
+//             for (let j = inputX - 1; j <= inputX + 1; j++) { //  X
+//                 try {    
+//                     if (i == inputY && j == inputX + 1) {
+//                         for (let j = inputX; j <= inputX; j++) { // X
+//                             matrix[i][j].style.backgroundColor = "white";
+//                             // el test de prueba da 'incorrecto' ya que no está planteada con white en el test, pero funciona correctamente
+//                         }
+//                     }
+//                     matrix[i][j].style.backgroundColor = "red";
+//                 }
+//                 catch {
+//                 }
+//             }
+//     } 
+// }
+
+function countNeighbours(x, y) {
+    let count = 0;
+    for (let i = y - 1; i <= y + 1; i++) { // Y
+        for (let j = x - 1; j <= x + 1; j++) { // X
+            try {
+                if (i==y && j==x) {
+                }
+                else if (matrix[i][j].style.backgroundColor == "red") {
+                    count++;
+                }
+            }
+            catch {
+            }
+        }
+    }
+    return count;
+}
+
+
+function minasAlrededor(event) { 
+    if (event.target.tagName == "TD") {
+        let inputX = parseInt(event.target.id);
+        let inputY = parseInt(event.target.className);
+    
+        for (let i = inputY - 1; i <= inputY + 1; i++) { //  Y
+            for (let j = inputX - 1; j <= inputX + 1; j++) { //  X
+                try {    
+                    count = countNeighbours(j, i);
+                    matrix[i][j].innerText = count;
+                }
+                catch {
+                }
+            }
+        }
     }
 }
